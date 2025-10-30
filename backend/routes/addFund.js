bebdca583ea5a097511198fdf2c9808e98b6902d
Vikash Sharma <vikashpandit380@gmail.com>
2025-10-30 11:59:25 +0530

    Vercel part update

--- backend/routes/addFund.js
+++ backend/routes/addFund.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/add-fund", async (req, res) => {
   try {
     const { userId, amount, redirect_url } = req.body;
 
