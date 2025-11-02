cfc485242b7b3233ee107c6ef58828a792c9730d
dristy13 <dristykumari13@gmail.com>
2025-11-02 11:24:59 +0530

    add nav and header in admin panel

--- Frontend/Admin/HeroSection.jsx
+++ Frontend/Admin/HeroSection.jsx
@@ -1,29 +1,25 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Styles";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
- 
 
 const HeroSection = () => {
-   const [balances, setBalances] = useState({
+  const [balances, setBalances] = useState({
     utilityBalance: 0,
-    apiBalance: 0
+    apiBalance: 0,
   });
 
   const fetchBalances = async () => {
     try {
-      const res = await fetch(
-        `${API_URL}/api/get-balance`
-      );
+      const res = await fetch(`${API_URL}/api/get-balance`);
       const data = await res.json();
       console.log("Fetched data:", data);
       console.log("API Balance:", data.apiBalance);
       console.log("Utility Balance:", data.utilityBalance);
 
-    setBalances({
-  utilityBalance: data.utilityBalance,
-  apiBalance: data.apiBalance
-});
-
+      setBalances({
+        utilityBalance: data.utilityBalance,
+        apiBalance: data.apiBalance,
+      });
     } catch (err) {
       console.error("Error fetching balances:", err);
     }
@@ -47,7 +43,9 @@ const HeroSection = () => {
               </div>
               <div style={styles.balanceItem}>
                 <span style={styles.balanceLabel}>Utility Bal:</span>
-                <span style={styles.balanceAmount}>₹{balances.utilityBalance}</span>
+                <span style={styles.balanceAmount}>
+                  ₹{balances.utilityBalance}
+                </span>
               </div>
             </div>
           </div>
